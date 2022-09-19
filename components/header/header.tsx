e6338be968c9abb41dc60426c71e03057d6478d4
{yunaeun} <naeun2964@naver.com>
2022-09-19 21:45:01 +0900

    ADD :: 메인 문제 컴포넌트 추가

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import * as S from "./style";
 import Link from "next/link";
 import Image from "next/image";
+
 const Header: NextPage = () => {
   return (
     <S.HeaderBox>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from "next";
-import * as S from "../styles/style";
-import "./../styles/Home.module.scss";
-const Home: NextPage = () => {
-  return <S.Container>메인</S.Container>;
+import * as S from "../styles/index-style";
+import ProblemLevel from "./problemlist/problemLevel";
+//npm install react-icons --save
+//yarn add react-icons
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.Title>문제목록</S.Title>
+      {["1", "2", "3", "4", "5"].map((data, idx) => {
+        return (
+          <ProblemLevel
+            key={idx}
+            level={data}
+            star={"star"}
+            complete={"complete"}
+          />
+        );
+      })}
+    </S.Container>
+  );
 };
 
 export default Home;
